c3510526a71214fba0db47858121c3e850dbcf4a
Chevallier_Q <quentinchevallier1@gmail.com>
2022-01-25 23:02:03 +0100

    First commit, add: login, signup and flux templates, login, signup and logout function

--- authentification/views.py
+++ authentification/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from re import template
+from tkinter.tix import Form
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from . import forms
+from django.conf import settings
+from django.views.generic import View
 
-# Create your views here.
+
+class LoginPage(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+        
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('flux')
+            else :
+                message = 'Identifiants invalides.'
+        return render(request, self.template_name, context={'form': form, 'message': message})
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('flux')
+    return render(request, 'authentication/signup.html', context={'form': form})
