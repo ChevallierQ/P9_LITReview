1b9a5828bae269dd1eb1c4da4f10c9cdf4b7ccf8
Chevallier_Q <quentinchevallier1@gmail.com>
2022-01-26 04:38:53 +0100

    Second commit, add: ticket and review templates and their function.

--- LITReview/urls.py
+++ LITReview/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 import authentification.views
 import litreview_app.views
 
@@ -10,4 +12,9 @@ urlpatterns = [
     path('signup/', authentification.views.signup_page, name='signup'),
     path('logout/', authentification.views.logout_page, name='logout'),
     path('flux/', litreview_app.views.home, name='flux'),
+    path('ticket/', litreview_app.views.ticket, name='ticket'),
+    path('review_without_ticket/', litreview_app.views.review_without_ticket, name='review_without_ticket')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- litreview_app/views.py
+++ litreview_app/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
+from pyexpat import model
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.template import context
+from . import models
+from . import forms
 
 
 @login_required
 def home(request):
     return render(request, 'flux/flux.html')
+  
+
+@login_required
+def ticket(request):
+    ticket_form = forms.TicketForm()
+    if request.method == 'POST':
+        ticket_form = forms.TicketForm(request.POST, request.FILES)
+        if ticket_form.is_valid():
+            ticket = ticket_form.save(commit=False)
+            ticket.user = request.user
+            ticket.save()
+            return redirect('flux')
+    context = {
+        'ticket_form': ticket_form,
+    }   
+    return render(request, 'ticket/ticket.html', context=context)
+
+
+@login_required
+def review_without_ticket(request):
+    ticket_form = forms.TicketForm()
+    review_without_ticket_form = forms.ReviewForm()
+    if request.method == 'POST':
+        ticket_form = ticket()
+        review_without_ticket_form = forms.ReviewForm(request.POST)
+        if all([ticket_form.is_valid(), review_without_ticket_form.is_valid()]):
+            review_without_ticket = review_without_ticket_form.save(commit=False)
+            review_without_ticket.user = request.user
+            review_without_ticket.save()
+            return redirect('flux')
+    context = {
+        'ticket_form': ticket_form,
+        'review_without_ticket_form': review_without_ticket_form
+    }
+    return render(request, 'review/review_without_ticket.html', context=context)
